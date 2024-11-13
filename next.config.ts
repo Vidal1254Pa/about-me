8f614a127bbba0f63f029816ed978030646f7fcc
Vidal1254Pa <58918751+Vidal1254Pa@users.noreply.github.com>
2024-11-13 17:53:19 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-    assetPrefix: isProd ? '/' : '',
+  /* config options here */
 };
 
 export default nextConfig;
